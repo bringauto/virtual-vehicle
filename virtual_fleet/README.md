a7bd90036593ccbef0285c392f48a9ddea91e80b
martin.hofbauer <martin.hofbauer@bringauto.com>
2023-02-09 11:14:22 +0000

    Resolve BAAS-491 "/update"

--- virtual_fleet/README.md
+++ virtual_fleet/README.md
@@ -1,7 +1,7 @@
 # Virtual fleet
 
-Virtual `VirtualFleet.py` provides ability to simulate car movement in bringauto industrial portal. Script will run one
-pair of VirtualVehicle and BADaemon for each entry in json and simulate its movement for Industrial portal (IP).
+Virtual fleet provides ability to simulate car movement in bringauto Fleet. Script will run one
+pair of VirtualVehicle and BADaemon for each entry in json and simulate its movement for Fleet.
 
 
 ## Docker version
@@ -18,62 +18,10 @@ Script [VirtualFleetDocker.py](VirtualFleetDocker.py) uses docker images to run 
 * `start-port` - starting port used for communication between vehicle and daemon, port will be iterated, so 8 virtual vehicles will use ports 42 000 - 42 007
 * `mqtt-address` - ip address of mqtt broker
 * `mqtt-port` - port of mqtt broker
+* `daemon-ip` - address, where is ba-daemon listening
 * `vehicles` - array of vehicles to create
 * `vehicles.name` - name of the vehicle, used for mqtt topics - `bringauto/default/<name>/<daemon|industrial_portal>`
 * `vehicles.docker-map-path` - path to map inside vehicle docker container used for simulation
 * `vehicles.route` - name of route used for simulation, route must exist in given map
 * `vehicles.stop-wait-seconds` - how many seconds should vehicle wait in stop
 * `vehicles.message-period-millisecond` - minimal period of status message
-
-## Non docker version
-Script [VirtualFleet.py](VirtualFleet.py) starts binaries at given path. Example settings are present in [json file](virtual_fleet.json)
-
-**You have to provide**
-
-- [Virtual Vehicle](https://gitlab.bringauto.com/bring-auto/host-platform/virtual-vehicle-utility) binary
-- [BringAuto daemon](https://gitlab.bringauto.com/jan.kubalek/bringauto-daemon) binary
-- json with Virtual Vehicle entries:
-    - mapPath - .osm file with named routes have to be provided
-    - name of vehicle - has to be unique and same correspond with vehicle in database in IP an token
-    - token - unique token, it has to be same with server, is unique for each car
-    - route - name of the route in the map file
-    - stopWaitTime - it defines, how many seconds should car stay on the stop
-
-Example of json file:
-
-```
-{
-  "virtualVehicles": [
-    {
-      "name": "BringAuto",
-      "mapPath": "/home/zak/Dokumenty/virtual-vehicle-utility/virtual_fleet/virtual_vehicle_map.osm",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDYXJJZCI6IjIiLCJDYXJOYW1lIjoiQnJpbmdBdXRvIiwiaXNzIjoiQnJpbmcuQXV0by5hcnRpbi5jeiIsImF1ZCI6IkJyaW5nLkF1dG8ifQ.6w11EkV3zKvG3Ly_CcF_SPsxSEbVEUOY9u0HvDw_Cr0",
-      "route": "brno_luzanky",
-      "stopWaitTime": 5
-    }
-  ]
-}
-```
-
-**Usage**
-
-Example run:
-`python3 VirtualFleet.py`
-`--deamon_path home/zak/Dokumenty/virtual-vehicle-utility/virtual_fleet/BAIndustrialPortal`
-`--virtual_path home/zak/Dokumenty/virtual-vehicle-utility/virtual_fleet/VirtualVehicle`
-`--json_path home/zak/Dokumenty/virtual-vehicle-utility/virtual_fleet/virtual_fleet.json`
-`--min_port 61234`
-
-Parameters:
-
-- --deamon_path PATH = path to daemon binary
-- --virtual_path PATH = path to virtual vehicle binary
-- --json_path PATH = path to json file
-- --min_port PORT, -mp PORT = starting port of the first entry from json file, each next entry will have automatically
-  min_port + 1 than the previous one
-
-One car will be simulated for each entry in json file. It will start his own daemon and virtual vehicle on new port.
-
-**Service**
-
-Included files in systemd directory. Before start, it needs change paths to the binary files.
--- virtual_fleet/VirtualFleetDocker.py
+++ virtual_fleet/VirtualFleetDocker.py
@@ -5,8 +5,11 @@ import sys
 import time
 import docker
 import logging
-from os import path
+import os
+import shutil
+import pathlib
 
+LOG_LAST_LINES = 5
 runningContainers = []
 
 
@@ -30,7 +33,7 @@ def parse_arguments():
 
 
 def check_paths(arguments):
-    if not path.exists(arguments.json_path):
+    if not os.path.exists(arguments.json_path):
         raise FileDoesntExistException("Json file " + arguments.json_path + " doesnt exist")
 
 
@@ -50,25 +53,22 @@ def run_program(arguments):
         port += 1
 
     logging.info("Start was successful")
-
     end = False
     while not end:
         for container in runningContainers:
             container.reload()
             if not container.attrs["State"]["Running"]:
-                logging.info("Error, container with id " + container.short_id + " is not running, app will exit,"
-                                                                                " container output: " + str(
-                    container.logs()))
-                stop_containers()
+                logging.info(f"Error, container with id {container.short_id} is not running, app will exit, container output: \n ==== \n{container.logs(tail=LOG_LAST_LINES).decode()} \n ==== \n")
                 end = True
-
-        time.sleep(5)
+        if not end: # don't want to wait 5 seconds, if containers crash in the beginning
+            time.sleep(5)
+    stop_containers()
 
 
 def start_containers(docker_client, settings, vehicle, port):
     daemon_container = docker_client.containers.run(
         settings["daemon-docker-image"] + ":" + settings["daemon-docker-tag"],
-        detach=True, network="host", auto_remove=True, entrypoint=[
+        detach=True, network="host", auto_remove=False, entrypoint=[
             "/bringauto-daemon/_build/components/industrial_portal/industrial_portal",
             '--vehicle-name=' + vehicle['name'], '--mode=mqtt', '--company=' + vehicle['company'], '--place=default',
             '--broker-port=' + str(settings["mqtt-port"]), '--broker-ip=' + settings["mqtt-address"],
@@ -80,11 +80,12 @@ def start_containers(docker_client, settings, vehicle, port):
 
     vehicle_container = docker_client.containers.run(
         settings["vehicle-docker-image"] + ":" + settings["vehicle-docker-tag"],
-        detach=True, network="host", auto_remove=True, entrypoint=[
+        detach=True, network="host", auto_remove=False, entrypoint=[
             "/virtual-vehicle-utility/virtual-vehicle-utility", '--map=' + vehicle['docker-map-path'],
-                                                       '--daemon-ip=' + "127.0.0.1",
+                                                       '--daemon-ip=' + settings["daemon-ip"],
                                                        '--wait=' + str(vehicle['stop-wait-seconds']),
                                                        '--period=' + str(vehicle['message-period-millisecond']),
+                                                       '--verbose',
                                                        '--daemon-port=' + str(port)])
     logging.info("Started a vehicle docker container with id " + vehicle_container.short_id)
     runningContainers.append(vehicle_container)
@@ -101,13 +102,21 @@ def exit_gracefully(signum, frame):
 
 
 def stop_containers():
+    log_dir = pathlib.Path("logs/")
+    if log_dir.exists():
+        shutil.rmtree(log_dir)
+    log_dir.mkdir() 
     for index, container in enumerate(runningContainers):
         logging.info("Stopping container with id " + container.short_id + " [" + str(index + 1) + "/"
                      + str(len(runningContainers)) + "]")
         try:
+            log_filename = f"{container.short_id}.log"
+            with open(log_dir/log_filename, "w") as text_file:
+                text_file.write(f"{container.logs().decode()}")
             container.stop()
         except:
             logging.error("Container " + container.short_id + " was already stopped")
+        
 
 
 if __name__ == '__main__':
@@ -123,5 +132,5 @@ if __name__ == '__main__':
         check_paths(args)
         run_program(args)
     except Exception as e:
-        print(str(e))
+        print(f"Main exception: {str(e)}")
         exit_gracefully(None, None)
