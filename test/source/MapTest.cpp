6ce006953bd0523df8af187842813a3d426c861e
melkus <josefmelkus@seznam.cz>
2023-05-24 13:44:11 +0200

    change test after different functional requirement

--- test/source/MapTest.cpp
+++ test/source/MapTest.cpp
@@ -11,13 +11,13 @@
 TEST_F(MapTest, MapCreation) {
 	std::string mapPath = "maps/virtual_vehicle_map.osm";
 	std::string emptyMapPath = "maps/empty.osm";
-	std::string wrongPath = "maps/fake.osm";
+	std::string wrongPath = "maps/fake_path.osm";
 
 	bringauto::virtual_vehicle::Map map1, map2, map3;
 	EXPECT_NO_THROW(map1.loadMapFromFile(mapPath));
 	EXPECT_ANY_THROW(map2.loadMapFromFile(emptyMapPath));
 	EXPECT_ANY_THROW(map3.loadMapFromFile(wrongPath));
 
-	EXPECT_ANY_THROW(map1.getRoute("fake route"));
+	EXPECT_NO_THROW(map1.getRoute("fake route"));
 	EXPECT_NO_THROW(map1.getRoute("brno_luzanky"));
 }
