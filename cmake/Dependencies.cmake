b4f90d515b3c549f80ea71f76c5425a462c21114
josef.melkus <josef.melkus@bringauto.com>
2024-04-02 11:22:36 +0000

    Fix dependencies and linking

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -8,10 +8,12 @@ BA_PACKAGE_LIBRARY(statesmurf       v2.0.1)
 BA_PACKAGE_LIBRARY(modbuspp         v0.3.1  PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(nlohmann-json    v3.10.5 PLATFORM_STRING_MODE any_machine NO_DEBUG ON)
 BA_PACKAGE_LIBRARY(internal-client  v1.0.0)
+BA_PACKAGE_LIBRARY(boost            v1.78.0)
 # All packages needed by libosmium for xml parsing
 BA_PACKAGE_LIBRARY(zlib             v1.2.11)
 BA_PACKAGE_LIBRARY(bzip2            v1.0.8)
 BA_PACKAGE_LIBRARY(expat            v2.4.8)
+BA_PACKAGE_LIBRARY(protozero        v1.7.1)
 BA_PACKAGE_LIBRARY(osmium           v2.17.3)
 
 IF (BRINGAUTO_TESTS)
